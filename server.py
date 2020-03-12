3c91b5a9cac111ca24b08447312543318a98fa87
Matevž Bencik <matevz.bencik@student.um.si>
2020-03-12 15:25:44 +0100

    Updating fixed

--- server.py
+++ server.py
@@ -5,14 +5,14 @@ import schedule
 import time
 
 app = Flask(__name__)
-sslify = SSLify(app)
+#sslify = SSLify(app)
 
 def get_data():
     global corona_response, country_response
-    country_response = requests.get("https://restcountries.eu/rest/v2/all?fields=name;population")
+    country_response = requests.get("https://restcountries.eu/rest/v2/all")
     corona_response = requests.get("https://lab.isaaclin.cn/nCoV/api/area?")
 
-schedule.every(10).minutes.do(get_data)
+schedule.every(1).minutes.do(get_data)
 
 country_response = requests.get("https://restcountries.eu/rest/v2/all")
 corona_response = requests.get("https://lab.isaaclin.cn/nCoV/api/area?")
