d7a5888a0fd7263541b1ec837d8e80b73b26a1c3
Matevž Bencik <matevz.bencik@student.um.si>
2020-03-12 02:22:33 +0100

    Second commit.

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, jsonify
+import requests
+import schedule
+import time
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+def get_data():
+    global corona_response, country_response
+    country_response = requests.get("https://restcountries.eu/rest/v2/all?fields=name;population")
+    corona_response = requests.get("https://lab.isaaclin.cn/nCoV/api/area?")
+
+schedule.every(10).minutes.do(get_data)
+
+country_response = requests.get("https://restcountries.eu/rest/v2/all")
+corona_response = requests.get("https://lab.isaaclin.cn/nCoV/api/area?")
+
+@app.route('/')
+def index():
+    return render_template("index.html", 
+        title="Pivička", 
+        corona_data=corona_response.text, 
+        country_data=country_response.text)
+
+if __name__ == '__main__':
+    app.run()
