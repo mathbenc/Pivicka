2e72070354968846975f5d2f7a4a4f40df24ea9c
Matevž Bencik <matevz.bencik@student.um.si>
2020-03-15 19:47:56 +0100

    Bug fix

--- server.py
+++ server.py
@@ -3,17 +3,15 @@ from flask_sslify import SSLify
 import requests
 import time
 import sys
-import atexit
-from apscheduler.scheduler import Scheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 #app.config['TEMPLATES_AUTO_RELOAD'] = True
 sslify = SSLify(app)
 
-cron = Scheduler(deamon=True)
-cron.start()
+sched = BackgroundScheduler(deamon=True)
+#scheduler = apscheduler.schedulers.blocking.BackgroundScheduler('apscheduler.job_defaults.max_instances': '2')
 
-@cron.interval_schedule(minutes=1)
 def get_data():
     global corona_response, country_response
     country_response = requests.get("https://restcountries.eu/rest/v2/all")
@@ -31,7 +29,8 @@ def index():
         corona_data=corona_response.text, 
         country_data=country_response.text)
 
-atexit.register(lambda: cron.shutdown(wait=False))
+sched.add_job(get_data, "cron", minute="*")
+sched.start()
 
 if __name__ == '__main__':
     app.run()
